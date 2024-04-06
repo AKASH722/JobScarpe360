735f8c1f24c227f723cbd5fa26ab0366963be4c8
AKASH RAI <akashrai4546@gmail.com>
2024-04-06 09:41:36 +0530

    Fixes

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from sqlalchemy import or_
-
 from flask import request, jsonify, render_template
+from sqlalchemy import or_, and_
 
 from connections import app, db
 from model import NaukriJob, IndeedJob, FlexJobsJob
@@ -33,50 +32,61 @@ def scrape_jobs():
 
 @app.route('/search-jobs', methods=['POST'])
 def search_jobs():
-    search_query = request.json.get('search_query')
-    location_query = request.json.get('location_query')
+    search_query: str = request.json.get('search_query')
+    location_query: str = request.json.get('location_query')
+    location = location_query.split()[0]
 
-    # Split search_query by space
     search_terms = search_query.split()
 
-    # Create an empty list to store search results
     naukri_jobs = []
     indeed_jobs = []
     flex_jobs = []
 
     # Query each job table for each search term and combine the results
     for term in search_terms:
-        naukri_jobs += NaukriJob.query.filter(or_(
-            NaukriJob.title.ilike(f'%{term}%'),
-            NaukriJob.location.ilike(f'%{term}%'),
-            NaukriJob.description.ilike(f'%{term}%')
-        )).all()
+        naukri_jobs += NaukriJob.query.filter(
+            and_(
+                or_(
+                    NaukriJob.title.ilike(f'%{term}%'),
+                    NaukriJob.description.ilike(f'%{term}%')
+                ),
+                NaukriJob.location.ilike(f'%{location}%')
+            )
+        ).all()
 
-        indeed_jobs += IndeedJob.query.filter(or_(
-            IndeedJob.title.ilike(f'%{term}%'),
-            IndeedJob.location.ilike(f'%{term}%'),
-            IndeedJob.description.ilike(f'%{term}%')
-        )).all()
+        indeed_jobs += IndeedJob.query.filter(
+            and_(
+                or_(
+                    IndeedJob.title.ilike(f'%{term}%'),
+                    IndeedJob.description.ilike(f'%{term}%')
+                ),
+                IndeedJob.location.ilike(f'%{location}%')
+            )
+        ).all()
 
-        flex_jobs += FlexJobsJob.query.filter(or_(
-            FlexJobsJob.title.ilike(f'%{term}%'),
-            FlexJobsJob.location.ilike(f'%{term}%'),
-            FlexJobsJob.description.ilike(f'%{term}%')
-        )).all()
+        flex_jobs += FlexJobsJob.query.filter(
+            and_(
+                or_(
+                    FlexJobsJob.title.ilike(f'%{term}%'),
+                    FlexJobsJob.description.ilike(f'%{term}%')
+                ),
+                FlexJobsJob.location.ilike(f'%{location}%')
+            )
+        ).all()
 
     return render_template('index.html', naukri_jobs=naukri_jobs, indeed_jobs=indeed_jobs, flex_jobs=flex_jobs)
 
 
 @app.route('/')
 def index():
-    naukri_jobs = NaukriJob.query.all()
-    indeed_jobs = IndeedJob.query.all()
-    flex_jobs = FlexJobsJob.query.all()
+    naukri_jobs = NaukriJob.query.limit(10).all()
+    indeed_jobs = IndeedJob.query.limit(10).all()
+    flex_jobs = FlexJobsJob.query.limit(10).all()
 
     return render_template('index.html', naukri_jobs=naukri_jobs, indeed_jobs=indeed_jobs, flex_jobs=flex_jobs)
 
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
     app.run()
